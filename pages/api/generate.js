2925ebd31ee1fa02d2a7ce41e19768a74f15508c
fygrav <gravelfy@pm.me>
2023-01-20 01:54:14 -0500

    first commit : synonymes

--- pages/api/generate.js
+++ pages/api/generate.js
@@ -1,4 +1,4 @@
-import { Configuration, OpenAIApi } from "openai";
+import { Configuration, OpenAIApi } from 'openai';
 
 const configuration = new Configuration({
   apiKey: process.env.OPENAI_API_KEY,
@@ -7,22 +7,26 @@ const openai = new OpenAIApi(configuration);
 
 export default async function (req, res) {
   const completion = await openai.createCompletion({
-    model: "text-davinci-002",
+    model: 'text-davinci-003',
     prompt: generatePrompt(req.body.animal),
+    max_tokens: 300,
     temperature: 0.6,
   });
   res.status(200).json({ result: completion.data.choices[0].text });
 }
 
-function generatePrompt(animal) {
-  const capitalizedAnimal =
-    animal[0].toUpperCase() + animal.slice(1).toLowerCase();
-  return `Suggest three names for an animal that is a superhero.
-
-Animal: Cat
-Names: Captain Sharpclaw, Agent Fluffball, The Incredible Feline
-Animal: Dog
-Names: Ruff the Protector, Wonder Canine, Sir Barks-a-Lot
-Animal: ${capitalizedAnimal}
-Names:`;
+function generatePrompt(requete) {
+  // const capitalizedAnimal =
+  //   requete.toUpperCase() + requete.slice(1).toLowerCase();
+  return `Suggérer des synonymes pour le mot suivant.
+Mot: Travailler
+Synonymes: Œuvrer, s'activer, s'employer, opérer, fonctionner, s'efforcer, s'appliquer, s'exercer, s'occuper, s'atteller, s'adonner, s'astreindre, se consacrer.
+Mot: ouvrage
+Synonymes: Livre, publication, écrit, manuscrit, traité, opuscule, recueil, volume, livret, brochure.
+Mot: murmurer
+Synonymes: babiller,  bafouiller,  balbutier,  baragouiner,  bougonner,  bourdonner,  bredouiller,  broncher,  bruire,  chanter,  couler,  dire,  frémir,  fredonner,  froufrouter,  gémir,  gazouiller,  geindre,  gringotter,  grognasser,  grogner,  grognonner,  grommeler,  gronder,  groumer,  mâchouiller,  marmonner,  marmotter,  maronner,  maugréer,  parler bas,  prononcer,  protester,  râler,  rechigner,  renauder,  rogner,  rognonner,  ronchonner,  ronfler,  rouscailler,  rouspéter,  se lamenter,  se plaindre,  souffler
+Mot: chanter
+Synonymes: beugler, bourdonner, brailler, chansonner, chantonner, conter, crier, débiter, dégoiser, détonner, dire, entonner, exécuter, exalter, extorquer, fredonner, gazouiller, glorifier, goualer, grésiller, gringotter, gueuler, jaser, louer, machicoter, miauler, murmurer, nasiller, nuancer, pépier, piauler, plaire, proclamer, psalmodier, publier, rabâcher, réciter, raconter, radoter, railler, ramager, répéter, redire, roucouler, s'égosiller, se glorifier, seriner, se vanter, siffler, solfier, sourire, striduler, ténoriser, tirer, vocaliser
+Mot: ${requete}
+Synonymes:`;
 }
--- pages/index.js
+++ pages/index.js
@@ -1,44 +1,44 @@
-import Head from "next/head";
-import { useState } from "react";
-import styles from "./index.module.css";
+import Head from 'next/head';
+import { useState } from 'react';
+import styles from './index.module.css';
 
 export default function Home() {
-  const [animalInput, setAnimalInput] = useState("");
+  const [animalInput, setAnimalInput] = useState('');
   const [result, setResult] = useState();
 
   async function onSubmit(event) {
     event.preventDefault();
-    const response = await fetch("/api/generate", {
-      method: "POST",
+    const response = await fetch('/api/generate', {
+      method: 'POST',
       headers: {
-        "Content-Type": "application/json",
+        'Content-Type': 'application/json',
       },
       body: JSON.stringify({ animal: animalInput }),
     });
     const data = await response.json();
     setResult(data.result);
-    setAnimalInput("");
+    setAnimalInput('');
   }
 
   return (
     <div>
       <Head>
-        <title>OpenAI Quickstart</title>
+        <title>Dictionnaire des synonymes (beta)</title>
         <link rel="icon" href="/dog.png" />
       </Head>
 
       <main className={styles.main}>
         <img src="/dog.png" className={styles.icon} />
-        <h3>Name my pet</h3>
+        <h3>Dictionnaire des synonymes (beta)</h3>
         <form onSubmit={onSubmit}>
           <input
             type="text"
             name="animal"
-            placeholder="Enter an animal"
+            placeholder="Entrer un mot."
             value={animalInput}
             onChange={(e) => setAnimalInput(e.target.value)}
           />
-          <input type="submit" value="Generate names" />
+          <input type="submit" value="Rechercher" />
         </form>
         <div className={styles.result}>{result}</div>
       </main>
